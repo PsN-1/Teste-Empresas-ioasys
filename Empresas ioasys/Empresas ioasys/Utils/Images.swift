a9b36f5eb9531c63007d1caaed26b12aa57b8292
Pedro Neto <pedro.santosnt@hotmail.com>
2022-01-05 18:58:44 -0300

    add project assets

---
+++ Empresas ioasys/Empresas ioasys/Utils/Images.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+enum Images {
+    static let backgroundImage = UIImage(named: "backgroundImage")
+    static let companyLogo = UIImage(named: "companyLogo")
+    static let companyLogoAndName = UIImage(named: "companyLogoAndName")
+    static let companyNotFound = UIImage(named: "companyNotFound")
+}
