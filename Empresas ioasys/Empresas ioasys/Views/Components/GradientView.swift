a9f98eae9fafc92c0f6d3447c229f8606e1b7f48
Pedro Neto <pedro.santosnt@hotmail.com>
2022-01-08 16:27:19 -0300

    add grandient background component

---
+++ Empresas ioasys/Empresas ioasys/Views/Components/GradientView.swift
@@ -0,0 +1,84 @@
+import UIKit
+
+class GradientView: UIView {
+    
+    override init(frame: CGRect = .zero){
+        super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false
+        setupView()
+        setupGradientView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    lazy var backgroundImage = buildBackgroundImage()
+    
+    func setupView() {
+        addSubview(backgroundImage)
+        NSLayoutConstraint.activate([
+            backgroundImage.topAnchor.constraint(equalTo: topAnchor),
+            backgroundImage.leadingAnchor.constraint(equalTo: leadingAnchor),
+            backgroundImage.trailingAnchor.constraint(equalTo: trailingAnchor),
+            backgroundImage.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+        ])
+        
+    }
+    
+    func setupGradientView() {
+        
+        let view = UILabel()
+        view.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+        view.backgroundColor = .white
+
+        view.layer.compositingFilter = "softLightBlendMode"
+        let layer0 = CAGradientLayer()
+        layer0.colors = [
+          UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor,
+          UIColor(red: 0.067, green: 0.067, blue: 0.067, alpha: 0).cgColor
+        ]
+        layer0.locations = [0.31, 0.98]
+        layer0.startPoint = CGPoint(x: 0.25, y: 0.5)
+        layer0.endPoint = CGPoint(x: 0.75, y: 0.5)
+        layer0.transform = CATransform3DMakeAffineTransform(CGAffineTransform(a: -1.4, b: 1.3, c: -1.3, d: -0.3, tx: 1.72, ty: 0.15))
+        layer0.bounds = view.bounds.insetBy(dx: -0.5*view.bounds.size.width, dy: -0.5*view.bounds.size.height)
+        layer0.position = view.center
+        view.layer.addSublayer(layer0)
+
+        addSubview(view)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+        view.heightAnchor.constraint(equalTo: heightAnchor).isActive = true
+        view.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0).isActive = true
+        view.topAnchor.constraint(equalTo: topAnchor, constant: 0).isActive = true
+    }
+}
+
+extension GradientView {
+    func buildBackgroundImage() -> UIImageView {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = Images.backgroundImage
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }
+    
+    func buildGenericLabel() -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .white
+        label.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+        
+        return label
+    }
+    
+    func buildTitle() -> UILabel {
+        let label = buildGenericLabel()
+        label.font = UIFont(name: "Gilroy-Bold", size: 40)
+        label.text = "Boas vindas,"
+        return label
+    }
+}
