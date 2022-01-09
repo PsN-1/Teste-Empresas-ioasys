8a7fc9bb85a51513afd1a4cbb4afac01cb160d82
Pedro Neto <pedro.santosnt@hotmail.com>
2022-01-09 19:22:04 -0300

    add image method on the network module

--- Empresas ioasys/Empresas ioasys/Networking/ApiClient.swift
+++ Empresas ioasys/Empresas ioasys/Networking/ApiClient.swift
@@ -1,16 +1,22 @@
 import Foundation
+import UIKit
 
 enum ApiMethod: String {
     case POST = "POST"
     case GET = "GET"
 }
 
+enum LoginResponse {
+    case success
+    case failed
+}
+
 enum UrlPath {
-    static let loginPath = "/users/auth/sign_in"
-    static let enterpriseIndexPath = "/enterprises?enterprise_types=3"
-    static let showByIdPath = "/enterprises/1"
-    static let enterprisesIndexWithFilterPath = "/enterprises?enterprise_types=1&name=aQm"
-    static let enterprisesWithNameFilterPath = "/enterprises?name=ioasys"
+    static let loginPath = "users/auth/sign_in"
+    static let enterpriseIndexPath = "enterprises?enterprise_types=3"
+    static let showByIdPath = "enterprises/"
+    static let enterprisesIndexWithFilterPath = "enterprises?enterprise_types=1&name=aQm"
+    static let enterprisesWithNameFilterPath = "enterprises?name=ioasys"
 }
 
 class ApiClient {
@@ -37,8 +43,8 @@ class ApiClient {
         return urlComponent
     }
     
-    func doPost(loginData: LoginModel) {
-        let loginUrl = "\(baseURL)/\(apiVersion)/users/auth/sign_in"
+    func doPost(loginData: LoginModel, completionHandler: @escaping ((LoginResponse) -> Void)) {
+        let loginUrl = "\(baseURL)/\(apiVersion)/\(UrlPath.loginPath)"
         let apiUrl = URL(string: loginUrl)!
         
         let request = buildRequest(
@@ -64,6 +70,13 @@ class ApiClient {
                     let uid = httpResponse.value(forHTTPHeaderField: "uid")
                     let client = httpResponse.value(forHTTPHeaderField: "client")
                     let accessToken = httpResponse.value(forHTTPHeaderField: "access-token")
+                    
+                    if let uid = uid,
+                       let client = client,
+                       let accessToken = accessToken {
+                        User.shared.setCredentials(uid: uid, client: client, accessToken: accessToken)
+                        completionHandler(LoginResponse.success)
+                    }
                 }
             }
         }
@@ -71,8 +84,82 @@ class ApiClient {
         task.resume()
     }
     
-    func doGet() {
+    func doGet(completionHandler: @escaping ((EnterpriseModel) -> Void)) {
+        let loginUrl = "\(baseURL)/\(apiVersion)/\(UrlPath.showByIdPath)"
+        let apiUrl = URL(string: loginUrl)!
         
+        let userCredentials = User.shared
+        let request = buildRequest(
+            with: apiUrl,
+            forMethod: .GET,
+            withHeaders: [
+                "Content-Type": "application/json",
+                "access-token": "\(userCredentials.accessToken ?? "")",
+                "client": "\(userCredentials.client ?? "")",
+                "uid": "\(userCredentials.uid ?? "")"
+            ]
+        )
+        
+        let task = session.dataTask(with: request) { data, response, error in
+            if error != nil {
+                
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                if httpResponse.statusCode == 200 {
+                    if let dataResponse = data {
+                        let jsonData = self.parse(json: dataResponse)
+                        completionHandler(jsonData)
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    func parse(json: Data) -> EnterpriseModel {
+        let decoder = JSONDecoder()
+        do {
+            let jsonData: EnterpriseModel = try decoder.decode(EnterpriseModel.self, from: json)
+            
+            print("Data Parsed: ")
+            print(jsonData)
+            return jsonData
+        } catch {
+            print("Error decoding Json")
+            return EnterpriseModel(enterprises: [Enterprise]())
+        }
+    }
+    
+    func doGetImage(for url: String, completionHandler: @escaping (() -> Void)) {
+        let company = Enterprise(photo: url)
+        doGetImages(for: [company]) {
+            completionHandler()
+        }
+    }
+    
+    func doGetImages(for companies: [Enterprise], completionHandler: @escaping (() -> Void)) {
+        let dispatchGroup = DispatchGroup()
+        let imagesUrls = companies.map( { $0.photo })
+       
+        imagesUrls.forEach { imageUrl in
+            dispatchGroup.enter()
+            if let url = URL(string: "https://empresas.ioasys.com.br\(imageUrl)") {
+                let task = session.dataTask(with: url) { data, response, error in
+                    
+                    if let data = data,
+                       let image = UIImage(data: data) {
+                        CachedImages.shared.saveToCache(image: image, for: imageUrl)
+                    }
+                    dispatchGroup.leave()
+                }
+                task.resume()
+            }
+        }
+        dispatchGroup.notify(queue: .main) {
+            completionHandler()
+        }
     }
 }
- 
+
+
