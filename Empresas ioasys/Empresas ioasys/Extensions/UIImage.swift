9db9fa0c421da134c134d8e0e2d04c595b583fdf
Pedro Neto <pedro.santosnt@hotmail.com>
2022-01-09 19:12:21 -0300

    add UIImage extension for resizing images

---
+++ Empresas ioasys/Empresas ioasys/Extensions/UIImage.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+extension UIImage {
+    private func resizedImage(_ newSize: CGSize) -> UIImage {
+        guard self.size != newSize else { return self }
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0);
+        self.draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+        let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+
+    func reduceImageSize(to rectSize: CGSize) -> UIImage {
+        let widthFactor = size.width / rectSize.width
+        let heightFactor = size.height / rectSize.height
+        
+        var resizeFactor = widthFactor
+        if size.height > size.width {
+            resizeFactor = heightFactor
+        }
+        let newSize = CGSize(width: size.width/resizeFactor, height: size.height/resizeFactor)
+        let resized = resizedImage(newSize)
+        return resized
+    }
+}
