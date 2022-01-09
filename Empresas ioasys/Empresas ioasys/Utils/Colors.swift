6b470cc317159ca972feacf6e34a9fcf26931a48
Pedro Neto <pedro.santosnt@hotmail.com>
2022-01-09 19:14:11 -0300

    create static constants for colors

---
+++ Empresas ioasys/Empresas ioasys/Utils/Colors.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+enum Colors {
+    static let grayLayerBorder = UIColor(red: 0.769, green: 0.769, blue: 0.769, alpha: 1)
+    static let placeHolderColor =  UIColor(red: 0.102, green: 0.106, blue: 0.11, alpha: 1)
+    static let lightGrayTitleColor = UIColor(red: 0.575, green: 0.575, blue: 0.575, alpha: 1)
+    static let purpleLayerBorder = UIColor(red: 0.598, green: 0.294, blue: 0.742, alpha: 1)
+}
