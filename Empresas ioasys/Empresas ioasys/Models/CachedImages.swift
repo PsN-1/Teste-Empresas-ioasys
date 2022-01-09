b11df75a89578b8d2b8a4d242c7976da08e6ee9d
Pedro Neto <pedro.santosnt@hotmail.com>
2022-01-09 19:13:21 -0300

    create the cache for downloaded images

---
+++ Empresas ioasys/Empresas ioasys/Models/CachedImages.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class CachedImages {
+    static let shared = CachedImages()
+
+    private let cache = NSCache<NSString, UIImage>()
+
+    func saveToCache(image: UIImage, for url: String) {
+        let imageUrl = NSString(string: url)
+
+        if cache.object(forKey: imageUrl) == nil {
+            cache.setObject(image, forKey: imageUrl)
+        }
+    }
+    
+    func getImageFor(url: String) -> UIImage? {
+        let imageUrl = NSString(string: url)
+        return cache.object(forKey: imageUrl)
+    }
+}
